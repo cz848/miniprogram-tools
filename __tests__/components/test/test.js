5cec83934219bf628dafa38227a07384863c853d
Tyler Chao <tyler.chao@verystar.cn>
2020-07-31 19:39:49 +0800

    test: miniprogram test

---
+++ __tests__/components/test/test.js
@@ -0,0 +1,12 @@
+import { mp } from '../../../index';
+
+mp.add(['login']);
+
+Component({
+  properties: {
+    title: {
+      type: String,
+      value: 'test Component',
+    }
+  },
+});
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
   testRegex: '/__tests__/.*?\\.test\\.js$',
   collectCoverage: true,
   collectCoverageFrom: ['./utils.js'],
+  // 使用 jest 内置 jsdom 进行 dom 环境的模拟
+  testEnvironment: 'jsdom',
+  // 配置 jest-snapshot-plugin 从而在使用 jest 的 snapshot 功能时获得更加适合肉眼阅读的结构
+  snapshotSerializers: ['miniprogram-simulate/jest-snapshot-plugin'],
 };
